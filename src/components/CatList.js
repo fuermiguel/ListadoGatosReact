b95e39a3dc73016224b5132cc9bb8b544329f1e9
cocoliso <fuermiguel@yahoo.es>
2019-02-18 12:29:26 +0000

    modificado al estilo ecmas6, y listo para entregar

--- src/components/CatList.js
+++ src/components/CatList.js
@@ -5,7 +5,7 @@ class CatList extends React.Component {
     render(){
         return(
             <section className="section-cats">
-                <h1>Todos los gatos son bonitos</h1>
+                <h1>Todos los gatos son bonitos.</h1>
                     <ul className="section-cats_list">
                         <li><RandomCat width={200} high={200} /></li>
                         <li><RandomCat  width={200} high={400}/></li>
--- src/components/RandomCat.js
+++ src/components/RandomCat.js
@@ -1,27 +1,19 @@
 import React from 'react';
 
-class RandomCat extends React.Component{
-    render(){
-        return(
-            <a href="http://lorempixel.com">
-                <img src={`http://lorempixel.com/${this.props.width}/${this.props.high}/cats`} alt="Random cat"/>
-            </a>
-        );
-    }
-}
+
+const RandomCat = ({width , high})=> (
+    <div>
+        <a href="http://lorempixel.com">
+            <img src={`http://lorempixel.com/${width}/${high}/cats`} alt="Random cat"/>
+        </a>
+    </div>
+);
+    
 
 //Definimos los valores por defecto de los props de la clase
 RandomCat.defaultProps = {
     width:400,
     high:200
 }
-
-// const RandomCat = ({width , high})=> {
-//     return(
-//         <a href="http://lorempixel.com">
-//             <img src="http://lorempixel.com/{width}/{high}/cats" alt="Random cat"/>
-//          </a>
-//     )};
-
 
 export default RandomCat;
